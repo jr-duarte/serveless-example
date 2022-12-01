aac448c80b3f677ab9ddcd68c0cba1f81d46e040
Junior Duarte <junior.duarte@casion.com.br>
2022-12-01 15:02:37 -0300

    chore: add valid yup

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,37 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
+import middy from 'middy';
+import { schemaValidator } from 'schemaValidator';
+import { object, string } from 'yup';
 
-export async function hello(
-  event: APIGatewayEvent,
-): Promise<APIGatewayProxyResult> {
-  console.log(event);
+async function hello(event: APIGatewayEvent): Promise<APIGatewayProxyResult> {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `testeeee ${process.env.TESTE}`,
+      message: `teste - env -${process.env.TESTE}`,
+      body: JSON.parse(event.body || ''),
+      params: event.queryStringParameters,
     }),
   };
 }
+
+export const testeSchema = {
+  body: object({
+    name: string()
+      .typeError('name - não é uma string')
+      .strict(true)
+      .required('name - campo obritagório'),
+    email: string()
+      .typeError('email - não é uma string')
+      .strict(true)
+      .email('email - inválido')
+      .required('email - campo obritagório'),
+  }).required(),
+  queryStringParameters: object({
+    id: string()
+      .typeError('id - não é uma string')
+      .strict(true)
+      .required('id - campo obritagório'),
+  }),
+};
+
+export const main = middy(hello).use(schemaValidator(testeSchema));
---
+++ src/schemaValidator.ts
@@ -0,0 +1,43 @@
+import { APIGatewayEvent } from 'aws-lambda';
+import { BaseSchema, ValidationError } from 'yup';
+
+type TRequest = {
+  event: APIGatewayEvent;
+};
+
+export const schemaValidator = (schema: {
+  body?: BaseSchema;
+  queryStringParameters?: BaseSchema;
+}) => {
+  const before = async (request: TRequest) => {
+    try {
+      const { body, queryStringParameters } = request.event;
+
+      if (schema.body) {
+        schema.body.validateSync(body, {
+          abortEarly: false,
+        });
+      }
+
+      if (schema.queryStringParameters) {
+        schema.queryStringParameters.validateSync(queryStringParameters ?? {}, {
+          abortEarly: false,
+        });
+      }
+
+      return Promise.resolve();
+    } catch (e) {
+      const error = e as ValidationError;
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          errors: error,
+        }),
+      };
+    }
+  };
+
+  return {
+    before,
+  };
+};
